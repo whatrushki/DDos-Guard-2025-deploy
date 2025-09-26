23a1fd80271d96e617e34744e1fa6e566998e095
nekrasovVM <NekraVM@yandex.ru>
2025-09-26 16:09:50 +0300

    Added GitManager.py and requirements.txt

---
+++ GitManager.py
@@ -0,0 +1,24 @@
+import os
+
+from git import Repo
+
+class GitManager:
+  def __init__(self, git_url, repo_dir):
+    self.git_url = git_url
+    self.repo_dir = repo_dir
+
+  def clone_repo(self):
+    if len(os.listdir(self.repo_dir)) != 0:
+      text = "Error: Target directory is not empty. Cloning interrupted"
+      print("\033[31m{}".format(text))
+      return
+
+    try:
+      Repo.clone_from(self.git_url, self.repo_dir)
+    except Exception as e: 
+      print(e)
+    else:
+      print("Repo cloned successfully!")
+
+# gitManager = GitManager("https://github.com/NekrasovVM/Bitrix.git", "/media/nekra/Acer/Ubuntu/source/test_dir")
+# gitManager.clone_repo()
